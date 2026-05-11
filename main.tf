cce8b067f1811492d863978e116e0f5072858186
Jan Kåre Eiken Reilstad <reilstad@gmail.com>
2026-05-11 11:58:46 +0200

    Add Azure connectivity module configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 
+module "connectivity" {
+  source  = "Azure/alz/connectivity/azurerm"
+  version = "~> 7.2"
+
+  location = "westeurope"
+
+  hub_virtual_networks = {
+    hub1 = {
+      address_space = ["10.100.0.0/16"]
+      firewall = {
+        enabled = true
+      }
+    }
+  }
+}
